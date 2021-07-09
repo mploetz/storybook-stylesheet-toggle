8b461ab5de0549bb513cc6ab9f80ce6420b64d0f
Matt Ploetz <mattploetz@yahoo.com>
2021-07-09 18:37:19 -0500

    constant

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const ADDON_ID = "storybook/stylesheet-toggle";
+export const ADDON_ID = "storybook/theme-selector";
 export const TOOL_ID = `${ADDON_ID}/tool`;
 export const PARAM_KEY = `themes`;
